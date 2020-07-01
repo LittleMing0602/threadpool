c6d1f9527d23bff3f0469a23977b906becdfd6eb
wanghengming <1239904635@qq.com>
2020-07-01 13:31:20 +0000

    add function getThreadMutex

--- Mutex.h
+++ Mutex.h
@@ -7,39 +7,48 @@ class MutexLock
 {
 public:
     MutexLock() {
-        int ret = pthread_mutex_init(&_mutex, NULL);
+        int ret = pthread_mutex_init(&mutex_, NULL);
         assert(ret == 0);
     }
     ~MutexLock() {
-        int ret = pthread_mutex_destroy(&_mutex);
+        int ret = pthread_mutex_destroy(&mutex_);
         assert(ret == 0);
     }
 
     void lock()
     {
-        pthread_mutex_lock(&_mutex);
+        pthread_mutex_lock(&mutex_);
     }
 
     void unlock()
     {
-        pthread_mutex_unlock(&_mutex);
+        pthread_mutex_unlock(&mutex_);
+    }
+
+    pthread_mutex_t* getPthreadMutex()
+    {
+        return &mutex_;
     }
 
 private:
-    pthread_mutex_t _mutex;
+    pthread_mutex_t mutex_;
 
 };
 
 //RAII
 class MutexLockGuard{
 public:
-    explicit MutexLockGuard(MutexLock& mutex):mutex_(mutex){
+    explicit MutexLockGuard(MutexLock& mutex):
+        mutex_(mutex)
+    {
         mutex_.lock();
     }
 
-    ~MutexLockGuard(){
+    ~MutexLockGuard()
+    {
         mutex_.unlock();
     }
+
 
 private:
     MutexLock& mutex_;
