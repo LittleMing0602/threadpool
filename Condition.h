bd071b446a13e410b3ab5594196853588a84ed33
wanghengming <1239904635@qq.com>
2020-07-01 13:31:50 +0000

    add Condition.h

---
+++ Condition.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Mutex.h"
+
+class Condition
+{
+public:
+    Condition(MutexLock& mutex):
+        mutex_(mutex)
+    {
+        pthread_cond_init(&cond_, NULL);
+
+    }
+
+    ~Condition() 
+    {
+        pthread_cond_destroy(&cond_);
+    }
+
+    void wait()
+    {
+        pthread_cond_wait(&cond_, mutex_.getPthreadMutex());
+    }
+
+    void notify()
+    {
+        pthread_cond_signal(&cond_);
+    }
+
+    void notifyAll()
+    {
+        pthread_cond_broadcast(&cond_);
+    }
+    
+
+private:
+    MutexLock& mutex_;
+    pthread_cond_t cond_;
+
+};
+
